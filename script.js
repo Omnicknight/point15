818fd942ca0484ffc7d26c769dc4cbced6bbeeb4
Vlad Zobnin <vladopodobniy@gmail.com>
2020-08-28 17:54:11 +0300

    fix number of arguments

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function sum(a,b,c,d) { return a+b+c+d; }
-
-const curry = (col, a = 0, b = 0) => {
-    return  function(c = 0, d = 0) {
-        return col(a, b, c, d);
+function sum(...args)  { return args.reduce(function(s, num) {
+    return s + num;
+  }, 0);
+};
+const curry = (col, ...arg1) => {
+    return  function(...arg2) {
+        return col(...arg1, ...arg2);
     }
 };
 
-const curriedFunction = curry(sum, 1, 2); // запоминает для вызова sum 2 параметра
+const curriedFunction = curry(sum, 1, 2, 6); // запоминает для вызова sum 2 параметра
 
-console.log(curriedFunction(3, 4)) // выводит 10 т.е. выполняется функционал sum(1,2,3,4).
+console.log(curriedFunction(3, 4, 6)) // выводит 10 т.е. выполняется функционал sum(1,2,3,4).
 
 
 const counter = (value = 0) => {
